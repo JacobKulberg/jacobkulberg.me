5ebef430a3ca2ba83ace185b93ea059622659f09
Jacob Kulberg <jacobkulberg@gmail.com>
2026-08-18 14:20:43 -0400

    Home: fix github contribution counter

--- home/js/main.js
+++ home/js/main.js
@@ -19,33 +19,14 @@ $(document).ready(function () {
 //* GITHUB CONTRIBUTIONS *//
 $(document).ready(async function () {
 	const username = 'JacobKulberg';
-	const startYear = 2021;
-	const today = new Date();
-	const currentYear = today.getFullYear();
-
-	// format to YYYY-MM-DD
-	const pad = (num) => String(num).padStart(2, '0');
-	const ymd = (y, m, d) => `${y}-${pad(m)}-${pad(d)}`;
-
-	async function fetchYearTotal(year) {
-		const from = ymd(year, 1, 1);
-		const to = year === currentYear ? ymd(today.getFullYear(), today.getMonth() + 1, today.getDate()) : ymd(year, 12, 31);
-
-		const url = `https://github-contributions-api.deno.dev/${username}.json?from=${from}&to=${to}`;
-		const res = await fetch(url, { headers: { Accept: 'application/json' } });
-		if (!res.ok) throw new Error(`API ${year} returned ${res.status}`);
-		const data = await res.json();
-
-		return data.totalContributions;
-	}
 
 	try {
-		const yearPromises = [];
-		for (let i = startYear; i <= currentYear; i++) {
-			yearPromises.push(fetchYearTotal(i));
-		}
-		const totals = await Promise.all(yearPromises);
-		const grandTotal = totals.reduce((a, b) => a + b);
+		const url = `https://github-contributions-api.jogruber.de/v4/${username}?y=all`;
+		const res = await fetch(url, { headers: { Accept: 'application/json' } });
+		if (!res.ok) throw new Error(`API returned ${res.status}`);
+		const data = await res.json();
+
+		const grandTotal = Object.values(data.total).reduce((a, b) => a + b, 0);
 
 		$('#github-contributions').text(grandTotal.toLocaleString());
 	} catch (err) {
